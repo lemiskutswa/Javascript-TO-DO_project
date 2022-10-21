fd0f2e9803294414c495452db5aa2dfb9198566d
lemiskutswa <kutswalemis16@gmail.com>
2022-10-21 22:32:27 +0300

    Functions and Arrays

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 element.innerText - takes the value of todo1 and places it in the div you've just created. 
 
 document.body.appendChild(element); - display the div on your page. 
+
+- You can find array methods by typing the array name followed by a period (for example todos.)
+
+- In Javascript, strings have methods too. 
--- script.js
+++ script.js
@@ -2,23 +2,21 @@ let todo1 = 'Wash wash';
 let todo2 = 'Buy Groceries';
 let todo3 = 'Wash car';
 
-let element = document.createElement('div');
-element.innerText = todo1;
-document.body.appendChild(element);
+function addTodo(todoTitle) {
+    let element = document.createElement('div');
+    element.innerText = todoTitle;
+    document.body.appendChild(element);
+} 
 
-element = document.createElement('div');
-element.innerText = todo2;
-document.body.appendChild(element);
+addTodo(todo1);
+addTodo(todo2);
+addTodo(todo3);
 
-element = document.createElement('div');
-element.innerText = todo3;
-document.body.appendChild(element);
+let todos = ['Wash car', 'Clean house','Wash utensils'];
+todos.push('cook');
 
-let button1 = 'Subscribe'
-element = document.createElement('button');
-element.innerText = button1;
-document.body.appendChild(element);
 
+console.log(todos);
 
 /*
 - document.createElement lets you create any HTML element but you have to specify it. 
